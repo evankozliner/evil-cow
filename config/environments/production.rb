8946a9441eea21e1ac448cfab492590b42feb695
evankozliner <evankozliner@gmail.com>
2016-11-08 15:02:05 -0500

    adding regular heroku url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-	config.web_socket_server_url = ["https://evil-cow.herokuapp.com/cable"]
+	config.web_socket_server_url = ["https://evil-cow.herokuapp.com/cable", "https://evil-cow.herokuapp.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
