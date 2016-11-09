6e33bf3e86407c114086d9e630bcb0b979566183
evankozliner <evankozliner@gmail.com>
2016-11-08 19:01:09 -0500

    Trying this w/ redis instead...

--- app/assets/javascripts/cable.js
+++ app/assets/javascripts/cable.js
@@ -8,6 +8,6 @@
 (function() {
   this.App || (this.App = {});
 
-  App.cable = ActionCable.createConsumer();
+  App.cable = ActionCable.createConsumer("Rails.application.config.web_socket_sever_url");
 
 }).call(this);
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,11 +34,12 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+	config.middleware.use ChatActionCable  
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.url = 'wss://evil-cow.herokuapp.com/cable'
+  config.action_cable.url = 'wss://evil-cow.herokuapp.com/'
 	config.allowed_request_origins = ['wss://evil-cow.herokuapp.com', 'wss://evil-cow.herokuapp.com/cable', "https://evil-cow.herokuapp.com/cable", "https://evil-cow.herokuapp.com"]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
